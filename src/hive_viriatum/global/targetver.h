f10d27db990ad6a58b05d30a415475bdbcd41cdc
joamag <joamag@gmail.com>
2010-12-18 20:23:41 +0000

    minor code change

--- src/hive_viriatum/global/targetver.h
+++ src/hive_viriatum/global/targetver.h
@@ -1,41 +1,43 @@
-// Hive Viriatum Web Server
-// Copyright (C) 2008 Hive Solutions Lda.
-//
-// This file is part of Hive Viriatum Web Server.
-//
-// Hive Viriatum Web Server is free software: you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
-//
-// Hive Viriatum Web Server is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-// GNU General Public License for more details.
-//
-// You should have received a copy of the GNU General Public License
-// along with Hive Viriatum Web Server. If not, see <http://www.gnu.org/licenses/>.
+/*
+ Hive Viriatum Web Server
+ Copyright (C) 2008 Hive Solutions Lda.
 
-// __author__    = João Magalhães <joamag@hive.pt>
-// __version__   = 1.0.0
-// __revision__  = $LastChangedRevision$
-// __date__      = $LastChangedDate$
-// __copyright__ = Copyright (c) 2008 Hive Solutions Lda.
-// __license__   = GNU General Public License (GPL), Version 3
+ This file is part of Hive Viriatum Web Server.
+
+ Hive Viriatum Web Server is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ Hive Viriatum Web Server is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with Hive Viriatum Web Server. If not, see <http://www.gnu.org/licenses/>.
+
+ __author__    = João Magalhães <joamag@hive.pt>
+ __version__   = 1.0.0
+ __revision__  = $LastChangedRevision$
+ __date__      = $LastChangedDate$
+ __copyright__ = Copyright (c) 2008 Hive Solutions Lda.
+ __license__   = GNU General Public License (GPL), Version 3
+*/
 
 #pragma once
 
-// sets windows version to windows xp and 2003 or above
+/* sets windows version to windows xp and 2003 or above */
 #ifndef WINVER
 #define WINVER 0x0501
 #endif
 
-// sets windows nt version to windows xp and 2003 or above
+/* sets windows nt version to windows xp and 2003 or above */
 #ifndef _WIN32_WINNT
 #define _WIN32_WINNT 0x0501
 #endif
 
-// sets windows 32 version to windows 98 or above
+/* sets windows 32 version to windows 98 or above */
 #ifndef _WIN32_WINDOWS
 #define _WIN32_WINDOWS 0x0410
 #endif
