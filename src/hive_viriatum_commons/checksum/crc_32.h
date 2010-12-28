be70b702174d67b02414fe1e292cb4a2c5d1f591
joamag <joamag@gmail.com>
2010-12-28 23:15:39 +0000

    minor makefile change

--- src/hive_viriatum_commons/checksum/crc_32.h
+++ src/hive_viriatum_commons/checksum/crc_32.h
@@ -86,4 +86,4 @@ static unsigned long crc32_tab[] = {
 };
 
 
-__attribute__ ((visibility("default"))) unsigned long crc32(unsigned char *buffer, unsigned int bufferLength);
+VIRIATUM_EXPORT_PREFIX2 unsigned long crc32(unsigned char *buffer, unsigned int bufferLength);
