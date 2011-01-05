15454cc4324d875603de43fe3e2f051a927ea762
João Magalhães <joamag@gmail.com>
2011-01-05 23:00:54 +0000

    new debug symbols added

--- src/hive_viriatum/viriatum.c
+++ src/hive_viriatum/viriatum.c
@@ -209,7 +209,7 @@ void runTests() {
     testBase64();
 }
 
-void service(char *filePath) {
+void service(int argc, char *argv[]) {
     /* allocates the socket data */
     SOCKET_DATA socketData;
 
@@ -240,6 +240,9 @@ void service(char *filePath) {
     /* allocates space for the number of bytes */
     size_t numberBytes;
 
+    /* allocates space for the file path */
+    char *filePath;
+
     /* allocates space for the file buffer */
     size_t fileSize;
 
@@ -259,6 +262,15 @@ void service(char *filePath) {
 
     /* allocates space for the select count */
     int selectCount;
+
+    /* in case the number of arguments is bigger than one */
+    if(argc > 1) {
+        /* sets the file path as the first argument */
+        filePath = argv[1];
+    } else {
+        /* sets the file path as a static file */
+        filePath = "C:\\Desert.jpg";
+    }
 
 	/* prints a debug message */
 	printf("Loading file '%s'\n", filePath);
@@ -395,26 +407,14 @@ void service(char *filePath) {
 }
 
 int main(int argc, char *argv[]) {
-    /* allocates space for the file path */
-    char *filePath;
+    /* runs the tests */
+    runTests();
 
 	/* prints a debug message */
 	printf("Receiving %d arguments\n", argc);
 
-    /* in case the number of arguments is bigger than one */
-    if(argc > 1) {
-        /* sets the file path as the first argument */
-        filePath = argv[1];
-    } else {
-        /* sets the file path as a static file */
-        filePath = "C:\\Desert.jpg";
-    }
-
-	/* runs the tests */
-    runTests();
-
     /* runs the service */
-    service(filePath);
+    service(argc, argv);
 
     /* returns zero (valid) */
     return 0;
