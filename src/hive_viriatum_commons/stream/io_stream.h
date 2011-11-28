56fa51f23e368dcd8f12aa1e17da846b9dae9191
joamag <joamag@gmail.com>
2011-11-28 21:56:10 +0000

    minor header refactor

--- src/hive_viriatum_commons/stream/io_stream.h
+++ src/hive_viriatum_commons/stream/io_stream.h
@@ -27,6 +27,8 @@
 
 #pragma once
 
+typedef struct Stream_t;
+
 typedef void (*streamUpdate) (struct Stream_t *stream);
 typedef size_t (*streamRead) (struct Stream_t *stream, unsigned char *buffer, size_t size);
 typedef size_t (*streamWrite) (struct Stream_t *stream, unsigned char *buffer, size_t size);
