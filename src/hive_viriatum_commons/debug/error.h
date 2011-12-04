8d7ab650af5e3689b1f0692296f2c5cbb8990ce3
João Magalhães <joamag@hive.pt>
2011-12-04 21:38:17 +0000

    new code refactor

--- src/hive_viriatum_commons/debug/error.h
+++ src/hive_viriatum_commons/debug/error.h
@@ -41,8 +41,8 @@
 #define IS_ERROR_CODE(errorCode) errorCode != 0
 #define GET_ERROR getLastErrorMessageSafe
 
-extern unsigned int lastErrorCode;
-extern unsigned char *lastErrorMessage;
+VIRIATUM_EXTERNAL_PREFIX unsigned int lastErrorCode;
+VIRIATUM_EXTERNAL_PREFIX unsigned char *lastErrorMessage;
 
 /**
  * Retrieves the last (current) error code available.
