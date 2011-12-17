daafc295a5fd23a3390c84534c255d53fb316dfb
joamag <joamag@gmail.com>
2011-12-17 04:24:31 +0000

    new refactor of headers

--- src/hive_viriatum_commons/stream/bit_stream.c
+++ src/hive_viriatum_commons/stream/bit_stream.c
@@ -23,7 +23,7 @@
  __date__      = $LastChangedDate$
  __copyright__ = Copyright (c) 2008 Hive Solutions Lda.
  __license__   = GNU General Public License (GPL), Version 3
- */
+*/
 
 #include "stdafx.h"
 
