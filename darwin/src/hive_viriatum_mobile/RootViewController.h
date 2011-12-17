ff5cb6f470da7d413c564e4f6a8ae64d0148ebae
João Magalhães <joamag@hive.pt>
2011-12-17 12:55:26 +0000

    new ios code structure

---
+++ darwin/src/hive_viriatum_mobile/RootViewController.h
@@ -0,0 +1,13 @@
+//
+//  RootViewController.h
+//  hive_viriatum_mobile
+//
+//  Created by João Magalhães on 12/17/11.
+//  Copyright 2011 Hive Solutions Lda. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RootViewController : UITableViewController
+
+@end
