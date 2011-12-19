a03555097b78126ade4e357868bcabe3226fc18b
joamag <joamag@gmail.com>
2011-12-19 00:12:15 +0000

    new definition added

--- src/hive_viriatum/global/definitions.h
+++ src/hive_viriatum/global/definitions.h
@@ -28,7 +28,9 @@
 #pragma once
 
 #ifdef VIRIATUM_PLATFORM_WIN32
+#ifndef VIRIATUM_MODULES_PATH
 #define VIRIATUM_MODULES_PATH "C:/repo_extra/viriatum/bin/hive_viriatum_mod_lua/i386/win32/Debug"
+#endif
 #define VIRIATUM_RESOURCES_PATH "../../src/hive_viriatum/resources/html/base"
 #define VIRIATUM_BASE_PATH "/"
 #define VIRIATUM_LISTING_PATH "/templates/listing.html.tpl"
