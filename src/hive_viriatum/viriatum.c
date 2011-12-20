7a411d470e9bb8673c5fb307013a0caa4ca8659b
joamag <joamag@gmail.com>
2011-12-20 10:36:54 +0000

    removed arguments print call

--- src/hive_viriatum/viriatum.c
+++ src/hive_viriatum/viriatum.c
@@ -147,7 +147,6 @@ int main(int argc, char *argv[]) {
 
     /* processes the various arguments into a map */
     processArguments(argc, argv, &arguments);
-    printArguments(arguments);
 
     /* runs the service, with the given arguments */
     returnValue = runService(arguments);
