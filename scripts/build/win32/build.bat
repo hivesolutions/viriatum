975ca00902a22789840a42350423187332f5b7e0
João Magalhães <joamag@gmail.com>
2012-05-02 12:16:13 +0100

    new build file

--- scripts/build/win32/build.bat
+++ scripts/build/win32/build.bat
@@ -1,24 +1,33 @@
 :: turns off the echo
 @echo off
 
+:: sets the version string constant value
+set VERSION=0.1.0
+
+:: sets the various global related name values
+:: (going to be used for file construction)
+set ARCHITECTURE=x86
+set NAME=viriatum-%VERSION%
+
 :: sets the various path related global
 :: variable with relative names
 set CURRENT_DIR=%cd%
 set BUILD_DIR=%CURRENT_DIR%\build
 set REPO_DIR=%BUILD_DIR%\repo
 set TARGET_DIR=%BUILD_DIR%\target
+set RESOURCES_DIR=%TARGET_DIR%\resources
+set SETUP_DIR=%TARGET_DIR%\setup
 set BASE_DIR=%REPO_DIR%
 set SRC_DIR=%BASE_DIR%\bin\hive_viriatum\i386\win32\Release
 set SOLUTION_DIR=%BASE_DIR%\win32\vs2008ex
-
-:: sets the version string constant value
-set VERSION=0.1.0
 
 :: creates the various directories that are going to be
 :: used during the build process
 mkdir %BUILD_DIR%
 mkdir %REPO_DIR%
 mkdir %TARGET_DIR%
+mkdir %RESOURCES_DIR%
+mkdir %SETUP_DIR%
 
 :: moves the current working directory to the build directory
 :: so that all the generated files are placed there
@@ -35,13 +44,20 @@ msbuild %SOLUTION_DIR%\hive_viriatum.sln /p:Configuration=Release
 :: changes the directory in order to group the files and then
 :: returns tho the "original" build directory
 cd %SRC_DIR%
-tar -cf viriatum_%VERSION%.tar index.html viriatum.exe resources templates
+tar -cf %NAME%.tar index.html viriatum.exe resources templates
+xcopy /q /e /k index.html %RESOURCES_DIR%
+xcopy /q /e /k viriatum.exe %RESOURCES_DIR%
+xcopy /q /a /e /k resources %RESOURCES_DIR%\resources
+xcopy /q /a /e /k templates %RESOURCES_DIR%\templates
+xcopy /q /e /k %NAME%.tar %RESOURCES_DIR%
 cd %BUILD_DIR%
+
+echo Building capsule setup package...
 
 :: runs the capsule process adding the viriatum group file
 :: to it in order to create the proper intaller
-capsule clone %TARGET_DIR%\viriatum-%VERSION%.exe
-capsule extend %TARGET_DIR%\viriatum-%VERSION%.exe Viriatum "Viriatum HTTP Server" %SRC_DIR%\viriatum_%VERSION%.tar
+capsule clone %SETUP_DIR%\%NAME%.exe
+capsule extend %SETUP_DIR%\%NAME%.exe Viriatum "Viriatum HTTP Server" %RESOURCES_DIR%\%NAME%.tar
 
 :: moves back to the current directory (back to the base)
 cd %CURRENT_DIR%
