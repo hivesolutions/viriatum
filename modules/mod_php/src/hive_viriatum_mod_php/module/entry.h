c878bc10d8cfc9b1fe3d0ce3e69c8b6f85029353
João Magalhães <joamag@gmail.com>
2012-05-07 13:29:52 +0100

    new php files

---
+++ modules/mod_php/src/hive_viriatum_mod_php/module/entry.h
@@ -0,0 +1,45 @@
+/*
+ Hive Viriatum Modules
+ Copyright (C) 2008 Hive Solutions Lda.
+
+ This file is part of Hive Viriatum Modules.
+
+ Hive Viriatum Modules is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ Hive Viriatum Modules is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with Hive Viriatum Modules. If not, see <http://www.gnu.org/licenses/>.
+
+ __author__    = João Magalhães <joamag@hive.pt>
+ __version__   = 1.0.0
+ __revision__  = $LastChangedRevision$
+ __date__      = $LastChangedDate$
+ __copyright__ = Copyright (c) 2008 Hive Solutions Lda.
+ __license__   = GNU General Public License (GPL), Version 3
+*/
+
+#pragma once
+
+#include "handler.h"
+
+/**
+ * The path to the file to be used as default
+ * in case no other definition exists (configuration).
+ */
+#define DEFAULT_FILE_PATH "default.php"
+
+/**
+ * Structure describing the internal
+ * structures and information for the
+ * mod php module.
+ */
+typedef struct ModPhpModule_t {
+	int tobias;
+} ModLuaModule;
