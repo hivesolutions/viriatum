5a77adb148a2eb231380edc0178b102f0d8e39fb
joamag <joamag@gmail.com>
2012-05-07 19:47:46 +0100

    new linked buffer

---
+++ src/hive_viriatum_commons/structures/linked_buffer.h
@@ -0,0 +1,59 @@
+/*
+ Hive Viriatum Commons
+ Copyright (C) 2008 Hive Solutions Lda.
+
+ This file is part of Hive Viriatum Commons.
+
+ Hive Viriatum Commons is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ Hive Viriatum Commons is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with Hive Viriatum Commons. If not, see <http://www.gnu.org/licenses/>.
+
+ __author__    = João Magalhães <joamag@hive.pt>
+ __version__   = 1.0.0
+ __revision__  = $LastChangedRevision$
+ __date__      = $LastChangedDate$
+ __copyright__ = Copyright (c) 2008 Hive Solutions Lda.
+ __license__   = GNU General Public License (GPL), Version 3
+*/
+
+#pragma once
+
+#include "buffer.h"
+#include "iterator.h"
+#include "linked_list.h"
+
+/**
+ * Structure holding internal information
+ * for a buffer (in memory) of buffers.
+ * The buffer is linear and contains the various
+ * partial buffer values.
+ */
+typedef struct LinkedBuffer_t {
+    /**
+     * The total length for the internal
+     * buffers value.
+     */
+    size_t bufferLength;
+
+    /**
+     * The list of buffers that compose the
+     * the complete buffer value.
+     */
+    struct LinkedList_t *bufferList;
+
+    /**
+     * The list of buffers to have the memory
+     * released uppon destruction of the linked
+     * buffer.
+     */
+    struct LinkedList_t *releaseList;
+} LinkedBuffer;
