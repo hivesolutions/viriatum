5318e8ebd1fa88448becfc8ac3c526ae38d0deb9
joamag <joamag@gmail.com>
2012-05-28 23:59:09 +0100

    initial bencoding structure

--- src/hive_viriatum_commons/encoding/bencoding.c
+++ src/hive_viriatum_commons/encoding/bencoding.c
@@ -29,6 +29,216 @@
 
 #include "bencoding.h"
 
+ERROR_CODE processBencodingFile(char *filePath, struct Type_t **typePointer) {
+	size_t stringSize;
+
+
+
+    /* allocates space for the general (temporary) variables
+    to be used durring the parsing of the file */
+    ERROR_CODE returnValue;
+    size_t index;
+    size_t fileSize;
+    unsigned char *fileBuffer;
+    unsigned char character;
+    enum BencodingState_e state;
+
+    /* allocates the mark variables used to locate
+    the part of context changing durring the parsing */
+    unsigned char *pointer = 0;
+    unsigned char *integerEndMark = 0;
+    unsigned char *stringEndMark = 0;
+
+    /* allocates space for the settings to be used by
+    the engine for the engine instance itself and for
+    the handler to be used to "catch" the events, then
+    retrieves the pointers to these structures*/
+    struct BencodingSettings_t bencodingSettings_s;
+    struct BencodingEngine_t bencodingEngine_s;
+    struct BencodingHandler_t bencodingHandler_s;
+    struct BencodingSettings_t *bencodingSettings = &bencodingSettings_s;
+    struct BencodingEngine_t *bencodingEngine = &bencodingEngine_s;
+    struct BencodingHandler_t *bencodingHandler = &bencodingHandler_s;
+
+    /* allocates space for the hash map to be used for
+    the configuration to be created */
+   /* struct HashMap_t *configuration;*/
+
+
+
+    /* creates the hash map that will hold the various
+    arguments, then updates the configuration pointer
+    with the created configuration hash map */
+   /* createHashMap(&configuration, 0);
+    *typePointer = configuration;*/
+
+
+
+
+    /* sets the various handlers for the bencoding settings
+    parsing, they will be used to correctly update the
+    provided configuration hash map */
+    bencodingSettings_s.onintegerStart = NULL;
+    bencodingSettings_s.onintegerEnd = _bencodingIntegerEndCallback;
+    bencodingSettings_s.onstringStart = NULL;
+    bencodingSettings_s.onstringEnd = _bencodingStringEndCallback;
+    bencodingSettings_s.onlistStart = _bencodingListStartCallback;
+    bencodingSettings_s.ondictionaryStart = _bencodingDictionaryStartCallback;
+    bencodingSettings_s.onsequenceEnd = _bencodingSequenceEndCallback;
+
+    /* sets the configuration reference in the bencoding handler
+    so that it may be updatd and then sets the handler in
+    the bencoding engine instance to be used for parsing */
+   
+	/* bencodingHandler_s.configuration = configuration; */
+    bencodingEngine_s.context = bencodingHandler;
+
+    /* reads the file contained in the provided file path
+    and then tests the error code for error, in case there is an
+    error prints it to the error stream output */
+    returnValue = readFile(filePath, &fileBuffer, &fileSize);
+    if(IS_ERROR_CODE(returnValue)) { RAISE_ERROR_M(RUNTIME_EXCEPTION_ERROR_CODE, (unsigned char *) "Problem reading file"); }
+
+    /* sets the initial state for the parsing process this
+    is considered to be the "general loop" state */
+    state = BENCODING_ENGINE_NORMAL;
+
+    /* iterates over the byte range of the file, all the bytes
+    should be contained in the buffer "under" iteration */
+    for(index = 0; index < fileSize; index++) {
+        /* retrieves the current character from the
+        file buffer and the retrieves the pointer to
+        its position */
+        character = fileBuffer[index];
+        pointer = &fileBuffer[index];
+
+        switch(state) {
+            case BENCODING_ENGINE_NORMAL:
+				switch(character) {
+					case 'i':
+						state = BENCODING_ENGINE_INTEGER;
+
+						BENCODING_MARK_N(integerEnd, -1);
+						BENCODING_CALLBACK(integerStart);
+
+						break;
+
+					case 'l':
+						BENCODING_CALLBACK(listStart);
+
+						break;
+
+					case 'd':
+						BENCODING_CALLBACK(dictionaryStart);
+
+						break;
+
+					case 'e':
+						BENCODING_CALLBACK(sequenceEnd);
+
+						break;
+
+					default:
+						state = BENCODING_ENGINE_STRING_SIZE;
+						stringSize = character - '0';
+
+						break;
+				}
+
+                break;
+
+            case BENCODING_ENGINE_INTEGER:
+                if(character == 'e') {
+                    state = BENCODING_ENGINE_NORMAL;
+
+                    BENCODING_CALLBACK_DATA(integerEnd);
+                }
+
+                break;
+
+            case BENCODING_ENGINE_STRING:
+                if(stringSize == 1) {
+                    state = BENCODING_ENGINE_NORMAL;
+
+                    BENCODING_CALLBACK_DATA_N(stringEnd, -1);
+				} else {
+					stringSize--;
+				}
+
+                break;
+
+            case BENCODING_ENGINE_STRING_SIZE:
+				if(character == ':') {
+                    state = BENCODING_ENGINE_STRING;
+
+					BENCODING_MARK_N(stringEnd, -1);
+					BENCODING_CALLBACK(stringStart);
+				} else {
+					stringSize *= 10;
+					stringSize += character - '0';
+				}
+
+                break;
+        }
+    }
+
+    /* releases the buffer used durring the parsing of
+    the configuration file */
+    FREE(fileBuffer);
+
+    /* raises no error */
+    RAISE_NO_ERROR;
+}
+
+ERROR_CODE _bencodingIntegerEndCallback(struct BencodingEngine_t *bencodingEngine, const unsigned char *pointer, size_t size) {
+    char *integer = malloc(size + 1);
+
+	memcpy(integer, pointer, size);
+	integer[size] = '\0';
+
+	printf("INTEGER -> %s\n", integer);
+
+	free(integer);
+
+    /* raises no error */
+    RAISE_NO_ERROR;
+}
+
+ERROR_CODE _bencodingStringEndCallback(struct BencodingEngine_t *bencodingEngine, const unsigned char *pointer, size_t size) {
+    char *string = malloc(size + 1);
+
+	memcpy(string, pointer, size);
+	string[size] = '\0';
+
+	printf("STRING -> %s\n", string);
+
+	free(string);
+
+	/* raises no error */
+    RAISE_NO_ERROR;
+}
+
+ERROR_CODE _bencodingListStartCallback(struct BencodingEngine_t *bencodingEngine) {
+	printf("NEW LIST\n");
+
+	/* raises no error */
+    RAISE_NO_ERROR;
+}
+
+ERROR_CODE _bencodingDictionaryStartCallback(struct BencodingEngine_t *bencodingEngine) {
+	printf("NEW Dictionary\n");
+
+	/* raises no error */
+    RAISE_NO_ERROR;
+}
+
+ERROR_CODE _bencodingSequenceEndCallback(struct BencodingEngine_t *bencodingEngine) {
+	printf("END SEQUENCE\n");
+
+	/* raises no error */
+    RAISE_NO_ERROR;
+}
+
 int encodeBencoding(unsigned char *buffer, size_t bufferLength, unsigned char **encodedBufferPointer, size_t *encodedBufferLengthPointer) {
     /* returns valid */
     return 0;
--- src/hive_viriatum_commons/encoding/bencoding.h
+++ src/hive_viriatum_commons/encoding/bencoding.h
@@ -27,5 +27,84 @@
 
 #pragma once
 
+#include "../io/io.h"
+#include "../debug/debug.h"
+#include "../structures/structures.h"
+
+#define BENCODING_MARK(FOR) BENCODING_MARK_N(FOR, 0)
+#define BENCODING_MARK_BACK(FOR) BENCODING_MARK_N(FOR, 1)
+#define BENCODING_MARK_N(FOR, N)\
+    do {\
+        FOR##Mark = pointer - N;\
+    } while(0)
+
+#define BENCODING_CALLBACK(FOR)\
+    do {\
+        if(bencodingSettings->on##FOR) {\
+            if(bencodingSettings->on##FOR(bencodingEngine) != 0) {\
+                RAISE_ERROR_M(RUNTIME_EXCEPTION_ERROR_CODE, (unsigned char *) "Problem handling callback"); \
+            }\
+        }\
+    } while(0)
+
+#define BENCODING_CALLBACK_DATA(FOR) BENCODING_CALLBACK_DATA_N(FOR, 0)
+#define BENCODING_CALLBACK_DATA_BACK(FOR) BENCODING_CALLBACK_DATA_N(FOR, 1)
+#define BENCODING_CALLBACK_DATA_N(FOR, N)\
+    do {\
+        if(FOR##Mark) {\
+            if(bencodingSettings->on##FOR) {\
+                if(bencodingSettings->on##FOR(bencodingEngine, FOR##Mark, pointer - FOR##Mark - N) != 0) {\
+                    RAISE_ERROR_M(RUNTIME_EXCEPTION_ERROR_CODE, (unsigned char *) "Problem handling callback"); \
+                }\
+            }\
+            FOR##Mark = NULL;\
+        }\
+    } while(0)
+
+struct BencodingEngine_t;
+
+typedef ERROR_CODE (*bencodingCallback) (struct BencodingEngine_t *);
+typedef ERROR_CODE (*bencodingDataCallback) (struct BencodingEngine_t *, const unsigned char *, size_t);
+
+typedef enum BencodingState_e {
+    BENCODING_ENGINE_NORMAL = 1,
+    BENCODING_ENGINE_INTEGER,
+    BENCODING_ENGINE_STRING,
+    BENCODING_ENGINE_LIST,
+    BENCODING_ENGINE_DICTIONARY,
+	BENCODING_ENGINE_STRING_SIZE
+} BencodingState;
+
+typedef struct BencodingSettings_t {
+    bencodingCallback onintegerStart;
+    bencodingDataCallback onintegerEnd;
+    bencodingCallback onstringStart;
+    bencodingDataCallback onstringEnd;
+    bencodingCallback onlistStart;
+    bencodingCallback ondictionaryStart;
+	bencodingCallback onsequenceEnd;
+} BencodingSettings;
+
+typedef struct BencodingEngine_t {
+    void *context;
+} BencodingEngine;
+
+typedef struct BencodingHandler_t {
+    struct HashMap_t *configuration;
+} BencodingHandler;
+
+VIRIATUM_EXPORT_PREFIX ERROR_CODE processBencodingFile(char *filePath, struct Type_t **typePointer);
+VIRIATUM_EXPORT_PREFIX ERROR_CODE _bencodingIntegerEndCallback(struct BencodingEngine_t *bencodingEngine, const unsigned char *pointer, size_t size);
+VIRIATUM_EXPORT_PREFIX ERROR_CODE _bencodingStringEndCallback(struct BencodingEngine_t *bencodingEngine, const unsigned char *pointer, size_t size);
+VIRIATUM_EXPORT_PREFIX ERROR_CODE _bencodingListStartCallback(struct BencodingEngine_t *bencodingEngine);
+VIRIATUM_EXPORT_PREFIX ERROR_CODE _bencodingDictionaryStartCallback(struct BencodingEngine_t *bencodingEngine);
+VIRIATUM_EXPORT_PREFIX ERROR_CODE _bencodingSequenceEndCallback(struct BencodingEngine_t *bencodingEngine);
+
+
+
+
+
+
+
 VIRIATUM_EXPORT_PREFIX int encodeBencoding(unsigned char *buffer, size_t bufferLength, unsigned char **encodedBufferPointer, size_t *encodedBufferLengthPointer);
 VIRIATUM_EXPORT_PREFIX int decodeBencoding(unsigned char *encodedBuffer, size_t encodedBufferLength, unsigned char **decodedBufferPointer, size_t *decodedBufferLengthPointer);
