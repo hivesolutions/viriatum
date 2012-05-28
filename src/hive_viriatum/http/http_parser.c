9c31885b103d2fb3417f4d3c552b814a53d35a25
joamag <joamag@gmail.com>
2012-05-28 12:24:24 +0100

    new support for request messages with no length

--- src/hive_viriatum/http/http_parser.c
+++ src/hive_viriatum/http/http_parser.c
@@ -1693,6 +1693,11 @@ int processDataHttpParser(struct HttpParser_t *httpParser, struct HttpSettings_t
         }
     }
 
+	if(state == STATE_BODY_IDENTITY_EOF) {
+		HTTP_CALLBACK(messageComplete);
+		state = NEW_MESSAGE();
+	}
+
     HTTP_CALLBACK_DATA(headerField);
     HTTP_CALLBACK_DATA(headerValue);
     HTTP_CALLBACK_DATA(url);
