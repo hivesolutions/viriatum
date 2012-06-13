ada2ceb63a8b4db3e8ba8054858405bd78c6857c
João Magalhães <joamag@gmail.com>
2012-06-13 16:15:13 +0100

    new wsgi

---
+++ modules/mod_wsgi/m4/exports.m4
@@ -0,0 +1,32 @@
+# Hive Viriatum Modules
+# Copyright (C) 2008 Hive Solutions Lda.
+#
+# This file is part of Hive Viriatum Modules.
+#
+# Hive Viriatum Modules is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Hive Viriatum Modules is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Hive Viriatum Modules. If not, see <http://www.gnu.org/licenses/>.
+
+# __author__    = João Magalhães <joamag@hive.pt>
+# __version__   = 1.0.0
+# __revision__  = $LastChangedRevision: 2390 $
+# __date__      = $LastChangedDate: 2009-04-02 08:36:50 +0100 (qui, 02 Abr 2009) $
+# __copyright__ = Copyright (c) 2008 Hive Solutions Lda.
+# __license__   = GNU General Public License (GPL), Version 3
+
+# exports the version definitions to automake
+MOD_WSGI_MAJOR_VERSION=1
+MOD_WSGI_MINOR_VERSION=0
+MOD_WSGI_MICRO_VERSION=0
+AC_SUBST(MOD_WSGI_MAJOR_VERSION)
+AC_SUBST(MOD_WSGI_MINOR_VERSION)
+AC_SUBST(MOD_WSGI_MICRO_VERSION)
---
+++ modules/mod_wsgi/src/hive_viriatum_mod_wsgi/stdafx.c
@@ -0,0 +1,36 @@
+/*
+ Hive Viriatum Modules
+ Copyright (C) 2008 Hive Solutions Lda.
+
+ This file is part of Hive Viriatum Modules.
+
+ Hive Viriatum Modules is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ Hive Viriatum Modules is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with Hive Viriatum Modules. If not, see <http://www.gnu.org/licenses/>.
+
+ __author__    = João Magalhães <joamag@hive.pt>
+ __version__   = 1.0.0
+ __revision__  = $LastChangedRevision$
+ __date__      = $LastChangedDate$
+ __copyright__ = Copyright (c) 2008 Hive Solutions Lda.
+ __license__   = GNU General Public License (GPL), Version 3
+*/
+
+#include "stdafx.h"
+
+static unsigned char name[] = "viriatum_mod_wsgi";
+static unsigned char version[] = "1.0.0";
+static unsigned char description[] = "Viriatum Wsgi Module";
+
+unsigned char *name_viriatum_mod_wsgi() { return name; }
+unsigned char *version_viriatum_mod_wsgi() { return version; }
+unsigned char *description_viriatum_mod_wsgi() { return description; }
