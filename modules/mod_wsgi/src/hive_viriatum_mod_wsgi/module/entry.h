4f3f7af28039572760cb07cbb4889f3547530031
João Magalhães <joamag@gmail.com>
2012-06-13 16:11:58 +0100

    new wsgi project

---
+++ modules/mod_wsgi/src/hive_viriatum_mod_wsgi/module/entry.h
@@ -0,0 +1,129 @@
+/*
+ Hive Viriatum Modules
+ Copyright (C) 2008 Hive Solutions Lda.
+
+ This file is part of Hive Viriatum Modules.
+
+ Hive Viriatum Modules is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ Hive Viriatum Modules is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with Hive Viriatum Modules. If not, see <http://www.gnu.org/licenses/>.
+
+ __author__    = João Magalhães <joamag@hive.pt>
+ __version__   = 1.0.0
+ __revision__  = $LastChangedRevision$
+ __date__      = $LastChangedDate$
+ __copyright__ = Copyright (c) 2008 Hive Solutions Lda.
+ __license__   = GNU General Public License (GPL), Version 3
+*/
+
+#pragma once
+
+#include "handler.h"
+
+/**
+ * The bae path to the directory to be used as default
+ * in case no other definition exists (configuration).
+ */
+#define DEFAULT_BASE_PATH "."
+
+/**
+ * Structure describing the internal
+ * structures and information for the
+ * mod wsgi module.
+ */
+typedef struct mod_wsgi_module_t {
+    /**
+     * The http handler associated with the
+     * module (upper layer).
+     */
+    struct http_handler_t *http_handler;
+
+    /**
+     * The mod wsgi http handler associated
+     * with the module.
+     */
+    struct mod_wsgi_http_handler_t *mod_wsgi_http_handler;
+} mod_wsgi_module;
+
+/**
+ * Structure representing a wsgi request
+ * for rendering of a page.
+ * This structure contains a series of
+ * values useful for request flush.
+ */
+typedef struct wsgi_request_t {
+    /**
+     * The mime type value for the current wsgi
+     * request, this header value is treated as
+     * a special case by the wsgi interpreter.
+     */
+    char mime_type[1024];
+
+    /**
+     * The matrix buffer containing the maximum
+     * possible count for various headers for the
+     * wsgi interpreter.
+     */
+    char headers[24][1024];
+
+    /**
+     * The number of headers currently present in
+     * the current wsgi request.
+     */
+    size_t header_count;
+
+    /**
+     * The reference to the current wsgi context
+     * structure in use.
+     */
+    struct handler_wsgi_context_t *wsgi_context;
+} wsgi_request;
+
+/**
+ * The global reference to the currently loaded service
+ * this is the reference required for the basic interaction
+ * with the service.
+ */
+struct service_t *_service;
+
+/**
+ * The global reference to the current connection being
+ * used, this is going to be used to access connection
+ * information values.
+ */
+struct connection_t *_connection;
+
+/**
+ * The global reference to the linked buffer to
+ * be used to hold the various strings resulting
+ * from the wsgi default execution output.
+ */
+struct linked_buffer_t *_output_buffer;
+
+/**
+ * The global structure to be used to "pass" wsgi
+ * information from the virtual machine into the
+ * appropriate viriatum request handler.
+ */
+struct wsgi_request_t _wsgi_request;
+
+VIRIATUM_EXPORT_PREFIX ERROR_CODE create_mod_wsgi_module(struct mod_wsgi_module_t **mod_wsgi_module_pointer, struct module_t *module);
+VIRIATUM_EXPORT_PREFIX ERROR_CODE delete_mod_wsgi_module(struct mod_wsgi_module_t *mod_wsgi_module);
+VIRIATUM_EXPORT_PREFIX ERROR_CODE start_module(struct environment_t *environment, struct module_t *module);
+VIRIATUM_EXPORT_PREFIX ERROR_CODE stop_module(struct environment_t *environment, struct module_t *module);
+VIRIATUM_EXPORT_PREFIX ERROR_CODE info_module(struct module_t *module);
+VIRIATUM_EXPORT_PREFIX ERROR_CODE error_module(unsigned char **message_pointer);
+VIRIATUM_EXPORT_PREFIX ERROR_CODE _load_configuration(struct service_t *service, struct mod_wsgi_http_handler_t *mod_wsgi_http_handler);
+VIRIATUM_EXPORT_PREFIX ERROR_CODE _load_wsgi_state();
+VIRIATUM_EXPORT_PREFIX ERROR_CODE _unload_wsgi_state();
+VIRIATUM_EXPORT_PREFIX ERROR_CODE _reload_wsgi_state();
+VIRIATUM_EXPORT_PREFIX ERROR_CODE _start_wsgi_state();
