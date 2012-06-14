70c486b84b1468e16071805ce2fa9a2b5c67fc33
João Magalhães <joamag@gmail.com>
2012-06-14 17:49:04 +0100

    initial python environment support

--- modules/mod_wsgi/src/hive_viriatum_mod_wsgi/module/handler.c
+++ modules/mod_wsgi/src/hive_viriatum_mod_wsgi/module/handler.c
@@ -64,7 +64,22 @@ ERROR_CODE create_handler_wsgi_context(struct handler_wsgi_context_t **handler_w
     /* set the default value in the handler wsgi context
     structure (resets the structure) */
     handler_wsgi_context->flags = 0;
-    handler_wsgi_context->iterator = NULL;
+    handler_wsgi_context->_next_header = UNDEFINED_HEADER;
+    handler_wsgi_context->_url_string.length = 0;
+    handler_wsgi_context->_file_name_string.length = 0;
+    handler_wsgi_context->_query_string.length = 0;
+    handler_wsgi_context->_file_path_string.length = 0;
+    handler_wsgi_context->_content_type_string.length = 0;
+    handler_wsgi_context->_content_length_string.length = 0;
+    handler_wsgi_context->_cookie_string.length = 0;
+    handler_wsgi_context->_host_string.length = 0;
+    handler_wsgi_context->_server_name_string.length = 0;
+	handler_wsgi_context->iterator = NULL;
+
+	/* sets the correct headers global reference in the wsgi
+	context and then resets the number of count to zero */
+	handler_wsgi_context->headers = &_headers;
+	handler_wsgi_context->headers->count = 0;
 
     /* sets the handler wsgi context in the  pointer */
     *handler_wsgi_context_pointer = handler_wsgi_context;
@@ -113,16 +128,160 @@ ERROR_CODE message_begin_callback_handler_module(struct http_parser_t *http_pars
 }
 
 ERROR_CODE url_callback_handler_module(struct http_parser_t *http_parser, const unsigned char *data, size_t data_size) {
+    /* retrieves the handler wsgi context from the http parser */
+    struct handler_wsgi_context_t *handler_wsgi_context = (struct handler_wsgi_context_t *) http_parser->context;
+
+    /* checks the position of the get parameters divisor position
+    and then uses it to calculate the size of the (base) path */
+    char *pointer = (char *) memchr((char *) data, '?', data_size);
+    size_t path_size = pointer == NULL ? data_size : pointer - (char *) data;
+    size_t query_size = pointer == NULL ? 0 : data_size - path_size - 1;
+    query_size = query_size > 0 ? query_size : 0;
+
+    /* copies the part of the data buffer relative to the file name
+    this avoids copying the query part */
+    memcpy(handler_wsgi_context->file_name, data, path_size);
+    handler_wsgi_context->file_name[path_size] = '\0';
+    normalize_path((char *) handler_wsgi_context->file_name);
+
+    /* in case the pointer is defined (query separator found) copies
+    the query contents into the target query buffer */
+    if(pointer) { memcpy(handler_wsgi_context->query, pointer + 1, query_size); }
+    handler_wsgi_context->query[query_size] = '\0';
+
+    /* copies the url to the url reference in the handler file context then
+    creates the file path from using the base viriatum path */
+    memcpy(handler_wsgi_context->url, data, data_size);
+    handler_wsgi_context->url[data_size] = '\0';
+    SPRINTF((char *) handler_wsgi_context->file_path, VIRIATUM_MAX_PATH_SIZE, "%s%s%s", VIRIATUM_CONTENTS_PATH, VIRIATUM_BASE_PATH, handler_wsgi_context->file_name);
+    normalize_path((char *) handler_wsgi_context->file_path);
+
+    /* populates the various generated strings, avoids possible recalculation
+    of the lengths of the string */
+    string_populate(&handler_wsgi_context->_file_name_string, handler_wsgi_context->file_name, path_size, 0);
+    string_populate(&handler_wsgi_context->_query_string, handler_wsgi_context->query, query_size, 0);
+    string_populate(&handler_wsgi_context->_url_string, handler_wsgi_context->url, path_size, 0);
+    string_populate(&handler_wsgi_context->_file_path_string, handler_wsgi_context->file_path, 0, 1);
+
     /* raise no error */
     RAISE_NO_ERROR;
 }
 
 ERROR_CODE header_field_callback_handler_module(struct http_parser_t *http_parser, const unsigned char *data, size_t data_size) {
+    /* retrieves the handler wsgi context from the http parser */
+    struct handler_wsgi_context_t *handler_wsgi_context = (struct handler_wsgi_context_t *) http_parser->context;
+
+    /* copies the current header name into the appropriate structure
+    and also updates the size of the name string in it */
+    handler_wsgi_context->header = &handler_wsgi_context->headers->values[handler_wsgi_context->headers->count];
+    memcpy(handler_wsgi_context->header->name, data, data_size);
+    handler_wsgi_context->header->name[data_size] = '\0';
+    handler_wsgi_context->header->name_size = data_size;
+
+    /* checks if the current header is a valid "capturable"
+    header in such case changes the next header value accordingly
+    otherwise sets the undefined header */
+    if(memcmp(data, "Content-Type", data_size) == 0) { handler_wsgi_context->_next_header = CONTENT_TYPE; }
+    else if(memcmp(data, "Content-Length", data_size) == 0) { handler_wsgi_context->_next_header = CONTENT_LENGTH; }
+    else if(memcmp(data, "Cookie", data_size) == 0) { handler_wsgi_context->_next_header = COOKIE; }
+    else if(memcmp(data, "Host", data_size) == 0) { handler_wsgi_context->_next_header = HOST; }
+    else { handler_wsgi_context->_next_header = UNDEFINED_HEADER; }
+
     /* raise no error */
     RAISE_NO_ERROR;
 }
 
 ERROR_CODE header_value_callback_handler_module(struct http_parser_t *http_parser, const unsigned char *data, size_t data_size) {
+    /* retrieves the handler wsgi context from the http parser */
+    struct handler_wsgi_context_t *handler_wsgi_context = (struct handler_wsgi_context_t *) http_parser->context;
+
+    /* allocates space for the pointer to be used for partial
+    calculation on the header values */
+    char *pointer;
+    size_t _data_size;
+
+    /* copies the current header value into the appropriate structure
+    and alse updates the size of the value string in it */
+    memcpy(handler_wsgi_context->header->value, data, data_size);
+    handler_wsgi_context->header->value[data_size] = '\0';
+    handler_wsgi_context->header->value_size = data_size;
+
+    /* adds the current header to the list of headers this is accomplished
+	by incrementing the current headers counter by one value*/
+    handler_wsgi_context->headers->count++;
+
+    /* switchs over the next header possible values to
+    copy the current header buffer into the appropriate place */
+    switch(handler_wsgi_context->_next_header) {
+        case CONTENT_TYPE:
+            /* copies the content type header value into the
+            appropriate buffer in the wsgi context */
+            memcpy(handler_wsgi_context->content_type, data, data_size);
+            handler_wsgi_context->content_type[data_size] = '\0';
+
+            /* populates the various generated strings, avoids possible recalculation
+            of the lengths of the string */
+            string_populate(&handler_wsgi_context->_content_type_string, handler_wsgi_context->content_type, data_size, 0);
+
+            /* breaks the switch */
+            break;
+
+        case CONTENT_LENGTH:
+            /* copies the content length header value into the
+            appropriate buffer in the wsgi context */
+            memcpy(handler_wsgi_context->content_length_, data, data_size);
+            handler_wsgi_context->content_length_[data_size] = '\0';
+
+            /* populates the various generated strings, avoids possible recalculation
+            of the lengths of the string */
+            string_populate(&handler_wsgi_context->_content_length_string, handler_wsgi_context->content_length_, data_size, 0);
+
+            /* breaks the switch */
+            break;
+
+        case COOKIE:
+            /* copies the cookie header value into the
+            appropriate buffer in the wsgi context */
+            memcpy(handler_wsgi_context->cookie, data, data_size);
+            handler_wsgi_context->cookie[data_size] = '\0';
+
+            /* populates the various generated strings, avoids possible recalculation
+            of the lengths of the string */
+            string_populate(&handler_wsgi_context->_cookie_string, handler_wsgi_context->cookie, data_size, 0);
+
+            /* breaks the switch */
+            break;
+
+        case HOST:
+            /* copies the host header value into the
+            appropriate buffer in the wsgi context */
+            memcpy(handler_wsgi_context->host, data, data_size);
+            handler_wsgi_context->host[data_size] = '\0';
+
+            /* tries to find the port part of the host in case
+            it's possible sets the base data size for the server name */
+            pointer = strchr((char *) handler_wsgi_context->host, ':');
+            if(pointer == NULL) { _data_size = data_size; }
+            else { _data_size = pointer - (char *) handler_wsgi_context->host; }
+
+            /* copies the server name header value into the
+            appropriate buffer in the wsgi context */
+            memcpy(handler_wsgi_context->server_name, data, _data_size);
+            handler_wsgi_context->server_name[_data_size] = '\0';
+
+            /* populates the various generated strings, avoids possible recalculation
+            of the lengths of the string */
+            string_populate(&handler_wsgi_context->_host_string, handler_wsgi_context->host, data_size, 0);
+            string_populate(&handler_wsgi_context->_server_name_string, handler_wsgi_context->server_name, _data_size, 0);
+
+            /* breaks the switch */
+            break;
+
+        default:
+            /* breaks the switch */
+            break;
+    }
+
     /* raise no error */
     RAISE_NO_ERROR;
 }
@@ -279,10 +438,26 @@ ERROR_CODE _send_response_handler_module(struct http_parser_t *http_parser) {
     (headers) part of the http message to be sent as response */
     char *headers_buffer;
 
+
+	char name[1024];
+	PyObject *_value;
+	PyObject *_value_;
+	struct http_header_value_t *header;
+
+
     /* retrieves the connection from the http parser parameters
     and then retrieves the handler wsgi context */
     struct connection_t *connection = (struct connection_t *) http_parser->parameters;
     struct handler_wsgi_context_t *handler_wsgi_context = (struct handler_wsgi_context_t *) http_parser->context;
+
+    /* retrieves the http method string value accessing the array of
+    static string values */
+    char *method = (char *) http_method_strings[http_parser->method - 1];
+
+
+
+    unsigned char *port = connection->service->options->_port;
+
 
     /* imports the wsgi module containing the util methos to be used by the
     application to access viriatum wsgi functions */
@@ -329,6 +504,97 @@ ERROR_CODE _send_response_handler_module(struct http_parser_t *http_parser) {
     environ = PyDict_New();
     PyTuple_SetItem(args, 0, environ);
     PyTuple_SetItem(args, 1, start_response_function);
+
+	/* iterates over all the heades in order to "export" them
+	into the environ dictionary to expose them to the application */
+	for(index = 0; index < _headers.count; index++) {
+		/* retrieves the current header structure for the
+		iteration cycle in course */
+		header = &_headers.values[index];
+
+		/* converts the current header name to uppercase
+		and appends the http prefix into it, then converts
+		the header value into a python string object */
+		uppercase(header->name);
+		SPRINTF(name, 1024, "HTTP_%s", header->name);
+		_value = PyString_FromString(header->value);
+
+		/* sets the value using the provided string name 
+		and decrements the reference count of the value */
+		PyDict_SetItemString(environ, name, _value);
+		Py_DECREF(_value);
+	}
+
+
+	_value_ = PyTuple_New(2);
+	_value = PyInt_FromLong(1);
+	PyTuple_SetItem(_value_, 0, _value);
+	_value = PyInt_FromLong(0);
+	PyTuple_SetItem(_value_, 1, _value);
+	PyDict_SetItemString(environ, "wsgi.version", _value_);
+	Py_DECREF(_value);
+	Py_DECREF(_value);
+	Py_DECREF(_value_);
+
+	_value = PyString_FromString("http");
+	PyDict_SetItemString(environ, "wsgi.url_scheme", _value);
+	Py_DECREF(_value);
+
+	_value = PyString_FromString("TENHO DE POR ALGUMA COISA");
+	PyDict_SetItemString(environ, "wsgi.input", _value);
+	Py_DECREF(_value);
+
+	_value = PyBool_FromLong(0);
+	PyDict_SetItemString(environ, "wsgi.multithread", _value);
+	Py_DECREF(_value);
+
+	_value = PyBool_FromLong(1);
+	PyDict_SetItemString(environ, "wsgi.multiprocess", _value);
+	Py_DECREF(_value);
+
+	_value = PyBool_FromLong(0);
+	PyDict_SetItemString(environ, "wsgi.run_once", _value);
+	Py_DECREF(_value);
+
+	_value = PyString_FromString(method);
+	PyDict_SetItemString(environ, "REQUEST_METHOD", _value);
+	Py_DECREF(_value);
+
+	_value = PyString_FromString(handler_wsgi_context->file_name);
+	PyDict_SetItemString(environ, "SCRIPT_NAME", _value);
+	Py_DECREF(_value);
+
+	_value = PyString_FromString(handler_wsgi_context->url);
+	PyDict_SetItemString(environ, "PATH_INFO", _value);
+	Py_DECREF(_value);
+
+	_value = PyString_FromString(handler_wsgi_context->query);
+	PyDict_SetItemString(environ, "QUERY_STRING", _value);
+	Py_DECREF(_value);
+
+	_value = PyString_FromString(handler_wsgi_context->server_name);
+	PyDict_SetItemString(environ, "SERVER_NAME", _value);
+	Py_DECREF(_value);
+
+	_value = PyString_FromString(port);
+	PyDict_SetItemString(environ, "SERVER_PORT", _value);
+	Py_DECREF(_value);
+
+	_value = PyString_FromString("HTTP/1.1");
+	PyDict_SetItemString(environ, "SERVER_PROTOCOL", _value);
+	Py_DECREF(_value);
+
+	if(handler_wsgi_context->_content_type_string.length > 0) {
+		_value = PyString_FromString(handler_wsgi_context->content_type);
+		PyDict_SetItemString(environ, "CONTENT_TYPE", _value);
+		Py_DECREF(_value);
+	}
+
+	if(handler_wsgi_context->_content_length_string.length > 0) {
+		_value = PyString_FromString(handler_wsgi_context->content_length_);
+		PyDict_SetItemString(environ, "CONTENT_LENGTH", _value);
+		Py_DECREF(_value);
+	}
 
     /* calls the handler function retrieving the result and releasing
     the resources immediately in case the result is not valid */
