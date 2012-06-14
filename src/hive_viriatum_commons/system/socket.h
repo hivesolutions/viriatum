ee2981fff8ae601f9490f035c8f0bd7e39382be7
joamag <joamag@gmail.com>
2012-06-14 12:05:32 +0100

    new no push definition

--- src/hive_viriatum_commons/system/socket.h
+++ src/hive_viriatum_commons/system/socket.h
@@ -76,6 +76,7 @@
 #define SOCKET_SET_CLEAR(socket_handle, sockets_set) FD_CLR(socket_handle, sockets_set)
 #define SOCKET_SET_IS_SET(socket_handle, sockets_set) FD_ISSET(socket_handle, sockets_set)
 #define SOCKET_SET_NON_BLOCKING(socket_handle, flags) SOCKET_IOCTL(socket_handle, FIONBIO, &flags)
+#define SOCKET_SET_NO_PUSH(socket_handle, flags)
 #endif
 
 #ifdef VIRIATUM_PLATFORM_UNIX
@@ -133,6 +134,7 @@
 #define SOCKET_SET_IS_SET(socket_handle, sockets_set) FD_ISSET(socket_handle, sockets_set)
 #define SOCKET_SET_NON_BLOCKING(socket_handle, flags) if((flags = SOCKET_FCNTL(socket_handle, F_GETFL, 0) == -1)) { flags = 0; }\
     SOCKET_FCNTL(socket_handle, F_SETFL, flags | O_NONBLOCK)
+#define SOCKET_SET_NO_PUSH(socket_handle, flags) flags = 1; SOCKET_SET_OPTIONS(socket_handle, SOCKET_PROTOCOL_TCP, TCP_CORK, &flags, sizeof(flags));
 #endif
 
 #ifdef VIRIATUM_PLATFORM_MSC
