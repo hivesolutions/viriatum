727f20f630f12cbe0d0ff68bfc4aa031e6442eba
João Magalhães <joamag@gmail.com>
2012-06-14 13:22:04 +0100

    new code fix

--- src/hive_viriatum/jni/jni_service.c
+++ src/hive_viriatum/jni/jni_service.c
@@ -45,7 +45,7 @@ jstring Java_pt_hive_viriatum_http_Service_run(JNIEnv *env, jclass cls) {
     /* runs the service, with the given arguments, they are
     crated on the fly as an empty map and deleted after*/
     create_hash_map(&arguments, 0);
-    return_value = run_service(arguments);
+    return_value = run_service("jni", arguments);
     delete_hash_map(arguments);
 
     /* tests the error code for error */
