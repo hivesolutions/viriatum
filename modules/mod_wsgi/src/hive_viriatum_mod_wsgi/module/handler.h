b939e2018defcbf9f5a10b2055a467b2d85eaaea
joamag <joamag@gmail.com>
2012-06-15 13:57:05 +0100

    module reload

--- modules/mod_wsgi/src/hive_viriatum_mod_wsgi/module/handler.h
+++ modules/mod_wsgi/src/hive_viriatum_mod_wsgi/module/handler.h
@@ -48,6 +48,14 @@ typedef struct mod_wsgi_http_handler_t {
      * and should never be used in production.
      */
     char reload;
+
+	/**
+  	 * The reference to the module object reprenting
+	 * the parsed and compiled file to be used.
+	 * This reference is used to avoid constant reloading
+	 * of the file in case the reload flag is not set.
+	 */ 
+	PyObject *module;
 } mod_wsgi_http_handler;
 
 typedef struct handler_wsgi_context_t {
