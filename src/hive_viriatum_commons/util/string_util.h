5e4c8914899d533c6052965320e327c186a8dd48
João Magalhães <joamag@gmail.com>
2012-06-18 16:18:09 +0100

    new split fnction support for string

--- src/hive_viriatum_commons/util/string_util.h
+++ src/hive_viriatum_commons/util/string_util.h
@@ -103,3 +103,56 @@ static __inline void normalize_path(char *string_value) {
         string_value++;
     }
 }
+
+static __inline void split(char *string_value, char *buffer, size_t size_e, char token) {
+	/* allocates space for the temporary token to be used to
+	store the current token in iteration */
+	char _token;
+
+	/* allocates space fo the size count the index counter
+	and the auxiliary (start) index counter */
+	size_t size;
+	size_t index = 0;
+	size_t _index = 0;
+
+	/* iterates continuousuly over the string value to correctly
+	parse the string value and split it */
+	while(1) {
+		/* retrieves the current token character to be evaluated
+		for conditional expression execution */
+		_token = string_value[index];
+
+		/* in case the current token matches the one to be used
+		as split, must split the string */
+		if(_token == token) {
+			/* calculates the size of the string from the difference
+			of index and then copies the string from one memory
+			buffer to the other over the calculated size */
+			size = index - _index;
+			memcpy(buffer, &string_value[_index], size);
+			buffer[size] = '\0';
+			
+			/* increments the buffer position with the size of one
+			buffer element and then re-sets the auxiliary buffer value */
+			buffer += size_e;
+			_index = index + 1;
+		}
+		/* otherwise in case the current token isthe end of string
+		the current string must be copied as the last trunk */
+		else if(_token == '\0') {
+			/* calculates the size of the string from the difference
+			of index and then copies the string from one memory
+			buffer to the other over the calculated size */
+			size = index - _index;
+			memcpy(buffer, &string_value[_index], size);
+			buffer[size] = '\0';
+
+			/* breaks the current loop (no more tokens to be split) */
+			break;
+		}
+
+		/* increments the current index value by one (buffer
+		iteration increment) */
+		index++;
+	}
+}
