390621220774d5e569ea61c0912b2b76cc029ba0
joamag <joamag@gmail.com>
2012-06-19 18:38:44 +0100

    minor bug fix

--- src/hive_viriatum/system/service.c
+++ src/hive_viriatum/system/service.c
@@ -748,7 +748,7 @@ ERROR_CODE start_service(struct service_t *service) {
 #endif
 
 #ifdef VIRIATUM_PLATFORM_UNIX
-   _socket6_address.sin6_len = sizeof(server);
+   _socket6_address.sin6_len = sizeof(_socket6_address);
    _socket6_address.sin6_family = SOCKET_INTERNET6_TYPE;
    _socket6_address.sin6_addr = in6addr_any;
    _socket6_address.sin6_port = htons(service_options->port);
