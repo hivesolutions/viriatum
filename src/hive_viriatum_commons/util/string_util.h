858b8e8081e46f3cb4843a6158d03d8fc9c56d22
joamag <joamag@gmail.com>
2012-06-20 18:12:54 +0100

    small code fix

--- src/hive_viriatum_commons/util/string_util.h
+++ src/hive_viriatum_commons/util/string_util.h
@@ -112,7 +112,7 @@ static __inline unsigned char *trim(unsigned char *string_value) {
 }
 
 static __inline unsigned char *untrim(unsigned char *string_value, char last) {
-	size_t length = (char *) strlen(string_value);
+	size_t length = strlen((char *) string_value);
 	if(length < 1) { return string_value; }
 	string_value[length] = last;
 	return string_value - 1;
