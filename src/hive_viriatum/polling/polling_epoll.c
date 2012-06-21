892b8db99feff417e35543750db5d66d9cdf6fd0
João Magalhães <joamag@gmail.com>
2012-06-21 17:50:30 +0100

    new epoll code

--- src/hive_viriatum/polling/polling_epoll.c
+++ src/hive_viriatum/polling/polling_epoll.c
@@ -28,3 +28,92 @@
 #include "stdafx.h"
 
 #include "polling_epoll.h"
+
+void create_polling_epoll(struct polling_epoll_t **polling_epoll_pointer, struct polling_t *polling) {
+    /* retrieves the polling epoll size */
+    size_t polling_epoll_size = sizeof(struct polling_epoll_t);
+
+    /* retrieves the connection pointer size */
+    size_t connection_pointer_size = sizeof(struct connection_t *);
+
+    /* allocates space for the polling epoll */
+    struct polling_epoll_t *polling_epoll = (struct polling_epoll_t *) MALLOC(polling_epoll_size);
+
+	/* creates the epoll file descriptor with an arbitrary
+	size (this no longer used by the kernel) */
+	polling_epoll->epoll_fd = epoll_create(1024);
+
+    /* resets the polling in the polling epoll */
+    polling_epoll->polling = polling;
+
+    /* sets the polling epoll in the polling epoll pointer */
+    *polling_epoll_pointer = polling_epoll;
+}
+
+void delete_polling_epoll(struct polling_epoll_t *polling_epoll) {
+	/* in case the polling fd is defined it must be closed
+	in order to avoid any memory leak problem */
+	if(polling_epoll->epoll_fd) { close(polling_epoll->epoll_fd); } 
+
+    /* releases the polling epoll */
+    FREE(polling_epoll);
+}
+
+ERROR_CODE open_polling_epoll(struct polling_t *polling) {
+    /* allocates the polling epoll */
+    struct polling_epoll_t *polling_epoll;
+
+    /* creates the polling epoll */
+    create_polling_epoll(&polling_epoll, polling);
+
+    /* sets the polling epoll in the polling as
+    the lower substrate */
+    polling->lower = (void *) polling_epoll;
+
+    /* raises no error */
+    RAISE_NO_ERROR;
+}
+
+ERROR_CODE close_polling_epoll(struct polling_t *polling) {
+    /* retrieves the polling epoll */
+    struct polling_epoll_t *polling_epoll = (struct polling_epoll_t *) polling->lower;
+
+    /* deletes the polling epoll */
+    delete_polling_epoll(polling_epoll);
+
+    /* raises no error */
+    RAISE_NO_ERROR;
+}
+
+ERROR_CODE register_connection_polling_epoll(struct polling_t *polling, struct connection_t *connection) {
+	/* allocates space for teh new event to be inserted into
+	the epoll polling system (this is an internal kernel structure) */
+	struct epoll_event _event;
+
+    /* retrieves the polling epoll structure from the upper
+	polling control structure */
+    struct polling_select_t *polling_epoll = (struct polling_select_t *) polling->lower;
+
+	/* populates the event structure with the appropriate
+	structures in order to register for the right events 
+	and then inserts the event request into the epoll fd */
+	_event.events = EPOLLIN | EPOLLOUT | EPOLLET;
+    _event.data.fd = connection->socket_handle;
+	epoll_ctl(polling_epoll->epoll_fd, EPOLL_CTL_ADD, connection->socket_handle, &_event);
+
+	/* raises no error */
+    RAISE_NO_ERROR;
+}
+
+ERROR_CODE unregister_connection_polling_select(struct polling_t *polling, struct connection_t *connection)  {
+    /* retrieves the polling epoll structure from the upper
+	polling control structure */
+    struct polling_select_t *polling_select = (struct polling_select_t *) polling->lower;
+
+
+	/*   !!!! PENDING IMPLEMENTATION    !!!!!! */
+
+
+    /* raises no error */
+    RAISE_NO_ERROR;
+}
--- src/hive_viriatum/polling/polling_epoll.h
+++ src/hive_viriatum/polling/polling_epoll.h
@@ -26,3 +26,27 @@
 */
 
 #pragma once
+
+#include "../system/system.h"
+
+typedef struct polling_epoll_t {
+    /**
+     * The polling reference, for top
+     * level reference.
+     */
+    struct polling_t *polling;
+
+    int epoll_fd;
+} polling_epoll;
+
+void create_polling_epoll(struct polling_epoll_t **polling_epoll_pointer, struct polling_t *polling);
+void delete_polling_epoll(struct polling_epoll_t *polling_epoll);
+ERROR_CODE open_polling_epoll(struct polling_t *polling);
+ERROR_CODE close_polling_epoll(struct polling_t *polling);
+
+
+ERROR_CODE register_connection_polling_epoll(struct polling_t *polling, struct connection_t *connection);
+ERROR_CODE unregister_connection_polling_epoll(struct polling_t *polling, struct connection_t *connection);
+ERROR_CODE register_write_polling_epoll(struct polling_t *polling, struct connection_t *connection);
+ERROR_CODE unregister_write_polling_epoll(struct polling_t *polling, struct connection_t *connection);
+
