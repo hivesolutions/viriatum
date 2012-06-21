888f3adcfb4598e2f1882f8c1167b703c0aaccc5
joamag <joamag@gmail.com>
2012-06-21 13:46:42 +0100

    new writing support

--- src/hive_viriatum/http/http_util.h
+++ src/hive_viriatum/http/http_util.h
@@ -37,6 +37,46 @@ struct connection_t;
 typedef ERROR_CODE (*_connection_data_callback) (struct connection_t *, struct data_t *, void *);
 
 /**
+ * The string representations for the various
+ * versions of the http protocol.
+ * These values are useful for usage in the
+ * status line printing part of the http message.
+ */
+static const char *http_version_codes[3] = {
+	"HTTP/0.9",
+	"HTTP/1.0",
+	"HTTP/1.1"
+};
+
+/**
+ * The buffer containing sequences of the
+ * various keep alive values.
+ */
+static const char *keep_alive_codes[2] = {
+	"close",
+	"keep-alive"
+};
+
+/**
+ * The various values for the cache control
+ * system values.
+ */
+static const char *cache_codes[1] = {
+	"no-cache, must-revalidate"
+};
+
+/**
+ * The buffer containing the various codes
+ * for possible header buffer closing.
+ * This values consider a boolean index as
+ * the driver index for printing.
+ */
+static const char *close_codes[2] = {
+	"",
+	"\r\n"
+};
+
+/**
  * The buffer containing sequences of the
  * descriptions to the various http error
  * codes orderes by major and minor parts
@@ -108,7 +148,10 @@ static const char *http_status_codes[5][64] = {
     }
 };
 
-ERROR_CODE write_http_error(struct connection_t *connection, char *header, char *error_code, char *error_message, char *error_description, _connection_data_callback callback, void *callback_parameters);
+ERROR_CODE write_http_headers(struct service_t *service, char *buffer, size_t size, enum http_version_e version, int status_code, char *status_message, enum http_keep_alive_e keep_alive, char close);
+ERROR_CODE write_http_headers_c(struct service_t *service, char *buffer, size_t size, enum http_version_e version, int status_code, char *status_message, enum http_keep_alive_e keep_alive, size_t content_length, enum http_cache_e cache, int close);
+ERROR_CODE write_http_headers_m(struct service_t *service, char *buffer, size_t size, enum http_version_e version, int status_code, char *status_message, enum http_keep_alive_e keep_alive, size_t content_length, enum http_cache_e cache, char *message);
+ERROR_CODE write_http_error(struct connection_t *connection, char *header, int error_code, char *error_message, char *error_description, _connection_data_callback callback, void *callback_parameters);
 ERROR_CODE log_http_request(char *host, char *identity, char *user, char *method, char *uri, char *protocol, int error_code, size_t content_length);
 
 __inline static const char *_get_http_status_code(size_t major, size_t minor) {
