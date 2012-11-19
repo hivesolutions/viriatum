9614092abcb2258b96d8502769eba6ca6678d564
joamag <joamag@gmail.com>
2012-11-19 22:46:04 +0000

    new support for verify

--- scripts/build/all/verify.py
+++ scripts/build/all/verify.py
@@ -6,18 +6,26 @@ import sys
 import shutil
 
 def run():
-    os.system("git clone git://github.com/hivesolutions/viriatum.git build")
+    # in case a version value was sent to the program
+    # must retrieve it to be validated, otherwise the
+    # version is set as invalid (unset)
+    if len(sys.argv) > 1: _version = sys.argv[1]
+    else: _version = None
+
+    os.system("git clone git://github.com/hivesolutions/viriatum.git verify")
     cwd = os.getcwd()
-    os.chdir("build")
+    os.chdir("verify")
     try:
         os.system("git rev-parse HEAD > VERSION")
         version_file = open("VERSION")
         try: version = version_file.read().strip()
         finally: version_file.close()
+        if _version: os.system("git log " + _version + "..HEAD --pretty=format:\"%h%x09%an%x09%ad%x09%s\" > LOG")
+        else: os.system("git log --pretty=format:\"%h%x09%an%x09%ad%x09%s\" > LOG")
     finally:
         os.chdir(cwd)
 
-    if version == sys.argv[1]: sys.exit(1)
+    if version == _version: sys.exit(1)
     else: sys.exit(0)  
 
 if __name__ == "__main__":
