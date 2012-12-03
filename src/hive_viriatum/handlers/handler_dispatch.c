f039f768fa653e5e36add4c3eb0ce9254808085b
joamag <joamag@gmail.com>
2012-12-03 23:37:43 +0000

    new dispatch handler support

--- src/hive_viriatum/handlers/handler_dispatch.c
+++ src/hive_viriatum/handlers/handler_dispatch.c
@@ -174,18 +174,6 @@ ERROR_CODE unset_handler_dispatch(struct http_connection_t *http_connection) {
 }
 
 ERROR_CODE message_begin_callback_handler_dispatch(struct http_parser_t *http_parser) {
-    /* retrieves the connection from the http parser parameters */
-    struct connection_t *connection = (struct connection_t *) http_parser->parameters;
-
-    /* retrieves the underlying connection references in order to be
-    able to operate over them, for unregister */
-    struct io_connection_t *io_connection = (struct io_connection_t *) connection->lower;
-    struct http_connection_t *http_connection = (struct http_connection_t *) io_connection->lower;
-
-    /* acquires the lock on the http connection, this will avoids further
-    messages to be processed, no parallel request handling problems */
-    http_connection->acquire(http_connection);
-
     /* raise no error */
     RAISE_NO_ERROR;
 }
@@ -444,6 +432,15 @@ ERROR_CODE _send_response_handler_dispatch(struct http_parser_t *http_parser) {
 
     /* retrieves the connection from the http parser parameters */
     struct connection_t *connection = (struct connection_t *) http_parser->parameters;
+
+    /* retrieves the underlying connection references in order to be
+    able to operate over them, for unregister */
+    struct io_connection_t *io_connection = (struct io_connection_t *) connection->lower;
+    struct http_connection_t *http_connection = (struct http_connection_t *) io_connection->lower;
+
+    /* acquires the lock on the http connection, this will avoids further
+    messages to be processed, no parallel request handling problems */
+    http_connection->acquire(http_connection);
 
     /* writes the response to the connection, registers for the appropriate callbacks
     this method uses the http error util to correctly format the error message */
