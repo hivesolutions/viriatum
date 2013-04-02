b99f899efc808378fad6f35774c2e0ba20639cc4
joamag <joamag@gmail.com>
2013-04-02 15:54:26 +0100

    new url file added

---
+++ src/hive_viriatum_commons/formats/url.c
@@ -0,0 +1,304 @@
+/*
+ Hive Viriatum Commons
+ Copyright (C) 2008-2012 Hive Solutions Lda.
+
+ This file is part of Hive Viriatum Commons.
+
+ Hive Viriatum Commons is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ Hive Viriatum Commons is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with Hive Viriatum Commons. If not, see <http://www.gnu.org/licenses/>.
+
+ __author__    = João Magalhães <joamag@hive.pt>
+ __version__   = 1.0.0
+ __revision__  = $LastChangedRevision$
+ __date__      = $LastChangedDate$
+ __copyright__ = Copyright (c) 2008-2012 Hive Solutions Lda.
+ __license__   = GNU General Public License (GPL), Version 3
+*/
+
+#include "stdafx.h"
+
+#include "url.h"
+
+ERROR_CODE parse_url(char *url, size_t url_size, struct url_t *url_s) {
+    size_t index;
+    size_t length;
+
+    unsigned char current;
+    enum url_parse_state_e state;
+
+    unsigned char *mark;
+    unsigned char *pointer;
+
+    unsigned char buffer[128];
+
+    mark = url;
+    state = SCHEME_STATE;
+
+    memset(url_s, 0, sizeof(struct url_t));
+
+    for(index = 0; index < url_size; index++) {
+        /* retrieves the current byte in iteration and the pointer
+        address to the current buffer position */
+        current = url[index];
+        pointer = &url[index];
+
+        switch(state) {
+            case SCHEME_STATE:
+                switch(current) {
+                    case ':':
+                        length = pointer - mark;
+                        url_s->scheme.buffer = mark;
+                        url_s->scheme.length = length;
+                        state = SLA_STATE;
+                        break;
+                }
+
+                break;
+
+            case SLA_STATE:
+                switch(current) {
+                    case '/':
+                        state = SLA_SLA_STATE;
+                        break;
+
+                    default:
+                        goto error;
+                        break;
+                }
+
+                break;
+
+            case SLA_SLA_STATE:
+                switch(current) {
+                    case '/':
+                        state = AUTH_STATE;
+                        mark = pointer + 1;
+                        break;
+
+                    default:
+                        goto error;
+                        break;
+                }
+
+                break;
+
+            case AUTH_STATE:
+                switch(current) {
+                    case '/':
+                        length = pointer - mark;
+                        url_s->host_c.buffer = mark;
+                        url_s->host_c.length = length;
+                        state = PATH_STATE;
+                        mark = pointer;
+                        break;
+
+                    case '@':
+                        length = pointer - mark;
+                        url_s->auth.buffer = mark;
+                        url_s->auth.length = length;
+                        state = HOST_STATE;
+                        mark = pointer + 1;
+                        break;
+                }
+
+                break;
+
+            case HOST_STATE:
+                switch(current) {
+                    case '/':
+                        length = pointer - mark;
+                        url_s->host.buffer = mark;
+                        url_s->host.length = length;
+                        state = PATH_STATE;
+                        mark = pointer;
+                        break;
+
+                    case ':':
+                        length = pointer - mark;
+                        url_s->host.buffer = mark;
+                        url_s->host.length = length;
+                        state = PORT_STATE;
+                        mark = pointer + 1;
+                        break;
+                }
+
+                break;
+
+            case PORT_STATE:
+                switch(current) {
+                    case '/':
+                        length = pointer - mark;
+                        url_s->port_s.buffer = mark;
+                        url_s->port_s.length = length;
+                        state = PATH_STATE;
+                        mark = pointer;
+                        break;
+                }
+
+                break;
+
+            case PATH_STATE:
+                switch(current) {
+                    case '?':
+                        length = pointer - mark;
+                        url_s->path.buffer = mark;
+                        url_s->path.length = length;
+                        state = QUERY_STATE;
+                        mark = pointer + 1;
+                        break;
+                }
+
+                break;
+
+            case QUERY_STATE:
+                switch(current) {
+                    case '#':
+                        length = pointer - mark;
+                        url_s->query.buffer = mark;
+                        url_s->query.length = length;
+                        state = FRAGMENT_STATE;
+                        mark = pointer + 1;
+                        break;
+                }
+
+                break;
+
+            case FRAGMENT_STATE:
+                break;
+        }
+    }
+    pointer++;
+
+    switch(state) {
+        case PATH_STATE:
+            length = pointer - mark;
+            url_s->path.buffer = mark;
+            url_s->path.length = length;
+            break;
+
+        case QUERY_STATE:
+            length = pointer - mark;
+            url_s->query.buffer = mark;
+            url_s->query.length = length;
+            break;
+
+        case FRAGMENT_STATE:
+            length = pointer - mark;
+            url_s->fragment.buffer = mark;
+            url_s->fragment.length = length;
+            break;
+
+        default:
+            goto error;
+            break;
+    }
+
+    if(url_s->host_c.buffer != NULL) {
+        pointer = memchr(url_s->host_c.buffer, ':', url_s->host_c.length);
+        if(pointer == NULL) {
+            url_s->host.buffer = url_s->host_c.buffer;
+            url_s->host.length = url_s->host_c.length;
+        } else {
+            url_s->host.buffer = url_s->host_c.buffer;
+            url_s->host.length = pointer - url_s->host_c.buffer;
+
+            url_s->port_s.buffer = pointer + 1;
+            url_s->port_s.length = url_s->host_c.buffer +\
+                url_s->host_c.length - (pointer + 1);
+        }
+    }
+
+    if(url_s->auth.buffer != NULL) {
+        pointer = memchr(url_s->auth.buffer, ':', url_s->auth.length);
+        if(pointer == NULL) { goto error; }
+
+        url_s->username.buffer = url_s->auth.buffer;
+        url_s->username.length = pointer - url_s->auth.buffer;
+
+        url_s->password.buffer = pointer + 1;
+        url_s->password.length = url_s->auth.buffer +\
+            url_s->auth.length - (pointer + 1);
+    }
+
+    if(url_s->port_s.buffer != NULL) {
+        memcpy(buffer, url_s->port_s.buffer, url_s->port_s.length);
+        buffer[url_s->port_s.length] = '\0';
+
+        url_s->port = atoi(buffer);
+    }
+
+    RAISE_NO_ERROR;
+
+error:
+    RAISE_ERROR_M(
+        RUNTIME_EXCEPTION_ERROR_CODE,
+        (unsigned char *) "Problem parsing url string"
+    );
+};
+
+ERROR_CODE parse_url_static(char *url, size_t url_size, struct url_static_t *url_s) {
+    /* allocates space for the temporary error variable to
+    be used to detect errors in calls */
+    ERROR_CODE error;
+
+    struct url_t _url_s;
+
+    error = parse_url(url, url_size, &_url_s);
+    if(error) {
+        RAISE_ERROR_M(
+            RUNTIME_EXCEPTION_ERROR_CODE,
+            (unsigned char *) "Problem parsing the url"
+        );
+    }
+
+    memset(url_s, 0, sizeof(struct url_static_t));
+
+    if(_url_s.scheme.buffer != NULL) {
+        memcpy(url_s->scheme, _url_s.scheme.buffer, _url_s.scheme.length);
+        url_s->scheme[_url_s.scheme.length] = '\0';
+    }
+
+    if(_url_s.username.buffer != NULL) {
+        memcpy(url_s->username, _url_s.username.buffer, _url_s.username.length);
+        url_s->username[_url_s.username.length] = '\0';
+    }
+
+    if(_url_s.password.buffer != NULL) {
+        memcpy(url_s->password, _url_s.password.buffer, _url_s.password.length);
+        url_s->password[_url_s.password.length] = '\0';
+    }
+
+    if(_url_s.host.buffer != NULL) {
+        memcpy(url_s->host, _url_s.host.buffer, _url_s.host.length);
+        url_s->password[_url_s.host.length] = '\0';
+    }
+
+    if(_url_s.port != 0) { url_s->port = _url_s.port; }
+
+    if(_url_s.path.buffer != NULL) {
+        memcpy(url_s->path, _url_s.path.buffer, _url_s.path.length);
+        url_s->password[_url_s.path.length] = '\0';
+    }
+
+    if(_url_s.query.buffer != NULL) {
+        memcpy(url_s->query, _url_s.query.buffer, _url_s.query.length);
+        url_s->password[_url_s.query.length] = '\0';
+    }
+
+    if(_url_s.fragment.buffer != NULL) {
+        memcpy(url_s->fragment, _url_s.fragment.buffer, _url_s.fragment.length);
+        url_s->password[_url_s.fragment.length] = '\0';
+    }
+
+    RAISE_NO_ERROR;
+}
