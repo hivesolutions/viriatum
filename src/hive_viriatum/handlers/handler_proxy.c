96eb22c80c54ead5faf3c80c61b0566f74fd1f6e
joamag <joamag@gmail.com>
2013-05-28 14:05:03 +0100

    small code fix

--- src/hive_viriatum/handlers/handler_proxy.c
+++ src/hive_viriatum/handlers/handler_proxy.c
@@ -791,7 +791,7 @@ ERROR_CODE body_callback_backend(struct http_parser_t *http_parser, const unsign
         (struct handler_proxy_context_t *) http_parser->context;
     struct connection_t *connection = handler_proxy_context->connection;
 
-	printf("body callback\m");
+	printf("body callback\n");
 
     char *buffer = MALLOC(data_size);
     memcpy(buffer, data, data_size);
