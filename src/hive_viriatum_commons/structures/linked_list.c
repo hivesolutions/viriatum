f0786ad198cd8400801ee20d550e1f836a25fb7c
joamag <joamag@gmail.com>
2013-05-28 13:14:03 +0100

    tiny code fix

--- src/hive_viriatum_commons/structures/linked_list.c
+++ src/hive_viriatum_commons/structures/linked_list.c
@@ -458,7 +458,7 @@ void peek_value_linked_list(struct linked_list_t *linked_list, void **value_poin
     the invalid value in the value pointer, otherwise
     sets the current top level value (first value) */
     if(linked_list_node == NULL) { *value_pointer = NULL; }
-    } else { *value_pointer = linked_list_node->value; }
+    else { *value_pointer = linked_list_node->value; }
 }
 
 void peek_top_value_linked_list(struct linked_list_t *linked_list, void **value_pointer) {
