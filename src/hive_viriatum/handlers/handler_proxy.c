f5d6e2edb7baaf64bbc898b6e96aa5ca5ddb1f1b
joamag <joamag@gmail.com>
2013-05-30 17:28:05 +0100

    new debug info

--- src/hive_viriatum/handlers/handler_proxy.c
+++ src/hive_viriatum/handlers/handler_proxy.c
@@ -651,6 +651,10 @@ ERROR_CODE _rabeton(struct connection_t *connection, struct data_t *data, void *
     struct connection_t *connection_c = handler_proxy_context->connection_c;
     handler_proxy_context->pending_write -= data->size;
 
+	if(connection_c->read_registered == FALSE) {
+		printf("%d\n", handler_proxy_context->pending_write);
+	}
+
     if(connection_c->read_registered == FALSE &&\
         handler_proxy_context->pending_write < VIRIATUM_TRE_READ) {
         connection_c->register_read(connection_c);
