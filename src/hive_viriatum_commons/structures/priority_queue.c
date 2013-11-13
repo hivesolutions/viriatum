98ec10bb9349151bf1e0682640e8b0188f28ad75
joamag <joamag@gmail.com>
2013-11-13 19:02:34 +0000

    new priority queue comments

--- src/hive_viriatum_commons/structures/priority_queue.c
+++ src/hive_viriatum_commons/structures/priority_queue.c
@@ -59,29 +59,52 @@ void delete_priority_queue(struct priority_queue_t *priority_queue) {
 }
 
 void push_priority_queue(struct priority_queue_t *priority_queue, void *value) {
-    struct iterator_t *iterator;
-    void *entry;
+    /* allocates the various variable that are going to be used
+    in the percolation of the linked list to find the correct
+    place to insert the new value in the list */
     int result;
+    void *entry;
+    struct iterator_t *iterator;
+
+    /* creates and starts the index position of the value at zero
+    as this is the fist position in the list (default one) */
     size_t index = 0;
 
+    /* creates the iterator for the list associated with the priority
+    queue and then iterates over it until no more items are left or the
+    right place to insert the value has been found */
     create_iterator_linked_list(priority_queue->list, &iterator);
     while(TRUE) {
+        /* tries to retrieve the next value from the iterator in case
+        none is returned breaks the current loop as the end of list has
+        been found and so the value must be inserted at the end of the list */
         get_next_iterator(iterator, (void **) &entry);
         if(entry == NULL) { break; }
 
+        /* uses the current comparator function to evaluate if the current
+        value is smaller that the current entry in case it is breaks the loop
+        as this is the right place to insert the new value */
         result = priority_queue->cmp(value, entry);
         if(result < 0) { break; }
 
+        /* increments the index with one more value as a new iteration loop
+        step has just finished */
         index++;
     }
 
+    /* deletes the pending iterator and adds the value to the underlying list
+    at the index that was just found through iteration */
     delete_iterator_linked_list(priority_queue->list, iterator);
     append_index_value_linked_list(priority_queue->list, value, index);
 
+    /* increments the size of the priority queue as one more element has
+    just been inserted into the queue */
     priority_queue->size++;
 }
 
 void pop_priority_queue(struct priority_queue_t *priority_queue, void **value_pointer) {
+    /* pops a value from the underlying list directly to the value pointer
+    and then decrements the size of the current priority queue */
     pop_value_linked_list(priority_queue->list, value_pointer, TRUE);
     priority_queue->size--;
 }
