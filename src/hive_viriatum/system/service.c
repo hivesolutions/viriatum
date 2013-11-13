52e83cbf5541e828a5a9c6f9575a5ed7e702bf30
joamag <joamag@gmail.com>
2013-11-13 11:45:37 +0000

    removed extra spaces

--- src/hive_viriatum/system/service.c
+++ src/hive_viriatum/system/service.c
@@ -1983,7 +1983,7 @@ ERROR_CODE _default_options_service(struct service_t *service, struct hash_map_t
     set_value_string_hash_map(service_options->mime_types, (unsigned char *) ".mht", "message/rfc822");
     set_value_string_hash_map(service_options->mime_types, (unsigned char *) ".mhtml", "message/rfc822");
     set_value_string_hash_map(service_options->mime_types, (unsigned char *) ".mid", "audio/mid");
-	set_value_string_hash_map(service_options->mime_types, (unsigned char *) ".mkv", "video/x-matroska");
+    set_value_string_hash_map(service_options->mime_types, (unsigned char *) ".mkv", "video/x-matroska");
     set_value_string_hash_map(service_options->mime_types, (unsigned char *) ".mny", "application/x-msmoney");
     set_value_string_hash_map(service_options->mime_types, (unsigned char *) ".mov", "video/quicktime");
     set_value_string_hash_map(service_options->mime_types, (unsigned char *) ".movie", "video/x-sgi-movie");
@@ -2077,7 +2077,7 @@ ERROR_CODE _default_options_service(struct service_t *service, struct hash_map_t
     set_value_string_hash_map(service_options->mime_types, (unsigned char *) ".wav", "audio/x-wav");
     set_value_string_hash_map(service_options->mime_types, (unsigned char *) ".wcm", "application/vnd.ms-works");
     set_value_string_hash_map(service_options->mime_types, (unsigned char *) ".wdb", "application/vnd.ms-works");
-	set_value_string_hash_map(service_options->mime_types, (unsigned char *) ".webm", "video/webm");
+    set_value_string_hash_map(service_options->mime_types, (unsigned char *) ".webm", "video/webm");
     set_value_string_hash_map(service_options->mime_types, (unsigned char *) ".wks", "application/vnd.ms-works");
     set_value_string_hash_map(service_options->mime_types, (unsigned char *) ".wmf", "application/x-msmetafile");
     set_value_string_hash_map(service_options->mime_types, (unsigned char *) ".wps", "application/vnd.ms-works");
