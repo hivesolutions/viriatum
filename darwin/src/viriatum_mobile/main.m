ca367e6e7c095235352f3ead1d42ffc0ca27d48e
joamag <joamag@gmail.com>
2015-01-05 16:48:03 +0000

    new year

--- darwin/src/viriatum_mobile/main.m
+++ darwin/src/viriatum_mobile/main.m
@@ -1,6 +1,6 @@
 /*
  Hive Viriatum Web Server
- Copyright (C) 2008-2014 Hive Solutions Lda.
+ Copyright (C) 2008-2015 Hive Solutions Lda.
 
  This file is part of Hive Viriatum Web Server.
 
@@ -21,7 +21,7 @@
  __version__   = 1.0.0
  __revision__  = $LastChangedRevision$
  __date__      = $LastChangedDate$
- __copyright__ = Copyright (c) 2008-2014 Hive Solutions Lda.
+ __copyright__ = Copyright (c) 2008-2015 Hive Solutions Lda.
  __license__   = GNU General Public License (GPL), Version 3
  */
 
