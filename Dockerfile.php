1977d0edf76fe8698c79dbedeb29fd44bf11b502
João Magalhães <joamag@gmail.com>
2026-02-07 13:59:36 +0000

    fix: add missing copy of /var/viriatum in Dockerfile

--- Dockerfile.php
+++ Dockerfile.php
@@ -57,6 +57,7 @@ COPY --from=builder /usr/lib/libviriatum.so.1.0.0 /usr/lib/libviriatum.so.1.0.0
 COPY --from=builder /usr/lib/libviriatum_http.so.1.0.0 /usr/lib/libviriatum_http.so.1.0.0
 COPY --from=builder /usr/lib/viriatum/modules/libviriatum_mod_php.so /usr/lib/viriatum/modules/libviriatum_mod_php.so
 COPY --from=builder /etc/viriatum /etc/viriatum
+COPY --from=builder /var/viriatum /var/viriatum
 
 RUN ln -s libviriatum.so.1.0.0 /usr/lib/libviriatum.so.1 && \
     ln -s libviriatum.so.1.0.0 /usr/lib/libviriatum.so && \
