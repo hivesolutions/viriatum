f6bc4f4d561de037e76371658222eddbdaff2ef4
João Magalhães <joamag@gmail.com>
2026-08-26 12:17:18 +0100

    fix: correct the line endings of the python extension files
    - the license header carried doubled carriage returns, rendering as
      extra blank lines in the editor
    - every new file now matches the header of the remaining sources byte
      for byte and uses plain CRLF endings

--- src/viriatum_python/module/server.c
+++ src/viriatum_python/module/server.c
@@ -1,25 +1,25 @@
-/*
- Hive Viriatum Web Server
- Copyright (c) 2008-2026 Hive Solutions Lda.
-
- This file is part of Hive Viriatum Web Server.
-
- Hive Viriatum Web Server is free software: you can redistribute it and/or modify
- it under the terms of the Apache License as published by the Apache
- Foundation, either version 2.0 of the License, or (at your option) any
- later version.
-
- Hive Viriatum Web Server is distributed in the hope that it will be useful,
- but WITHOUT ANY WARRANTY; without even the implied warranty of
- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- Apache License for more details.
-
- You should have received a copy of the Apache License along with
- Hive Viriatum Web Server. If not, see <http://www.apache.org/licenses/>.
-
- __author__    = João Magalhães <joamag@hive.pt>
- __copyright__ = Copyright (c) 2008-2026 Hive Solutions Lda.
- __license__   = Apache License, Version 2.0
+/*
+ Hive Viriatum Web Server
+ Copyright (c) 2008-2026 Hive Solutions Lda.
+
+ This file is part of Hive Viriatum Web Server.
+
+ Hive Viriatum Web Server is free software: you can redistribute it and/or modify
+ it under the terms of the Apache License as published by the Apache
+ Foundation, either version 2.0 of the License, or (at your option) any
+ later version.
+
+ Hive Viriatum Web Server is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ Apache License for more details.
+
+ You should have received a copy of the Apache License along with
+ Hive Viriatum Web Server. If not, see <http://www.apache.org/licenses/>.
+
+ __author__    = João Magalhães <joamag@hive.pt>
+ __copyright__ = Copyright (c) 2008-2026 Hive Solutions Lda.
+ __license__   = Apache License, Version 2.0
 */
 
 #include "stdafx.h"
